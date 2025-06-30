948f84d2b50ecab3730a0c8adc4239bd2c37d58f
Thangwibu1 <133678875+Thangwibu1@users.noreply.github.com>
2025-06-30 22:16:42 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <Math.h>
+// Blynk library for ESP32
 
 #define BLYNK_TEMPLATE_ID "TMPL6kaLmF0Ic"
 #define BLYNK_TEMPLATE_NAME "Test"
@@ -12,50 +14,44 @@
 char ssid[] = "3000 TEA - NGOGIATU"; // Replace with your WiFi SSID
 char pass[] = "0827110100";
 
-int LED = 2;
+int LEDAlert = 2;
+int LEDWarning = 4;
 int Sensor = 32;
 int Flame = 33;
-int HumanDetected = 23;
+int relayPump = 26;
+const int gasDefault = analogRead(Sensor);
 BlynkTimer timer;
 
 void sendSensor() {
   int sensorValue = analogRead(Sensor);
   int flameValue = analogRead(Flame);
-  int humanDetected = digitalRead(HumanDetected);
-  Serial.print("Sensor Value: ");
-  Serial.println(sensorValue);
-  Serial.print("Flame Value: ");
-  Serial.println(flameValue);
+
   Blynk.virtualWrite(V1, sensorValue);
   Blynk.virtualWrite(V2, flameValue);
-  if (sensorValue > 2000 || flameValue < 1000) {
-    digitalWrite(LED, HIGH);
-    Serial.println("Warning: Sensor or Flame Detected!");
-    Blynk.virtualWrite(V0, HIGH);
-    digitalWrite(LED, HIGH);
-    Blynk.logEvent("Warning: Sensor or Flame Detected!");
-    if (humanDetected == HIGH) {
-      Serial.println("Human Detected: Sending Alert!");
-    } else {
-      Serial.println("No Human Detected: Sending Alert!");
-    }
-    
+
+  if (Math.abs(sensorValue - gasDefault) > 100) { // Adjust threshold as needed
+    digitalWrite(LEDWarning, HIGH);
+    Blynk.logEvent("gas_leak","Gas leak detected!");
+    Blynk.virtualWrite(V3, HIGH); // Check if gas leak detected
   } else {
-    digitalWrite(LED, LOW);
-    Serial.println("No Warning: Sensor and Flame Normal.");
-    Blynk.virtualWrite(V0, LOW);
-    Blynk.logEvent("No Warning: Sensor and Flame Normal.");
-    if (humanDetected == HIGH) {
-      Serial.println("Human Detected: Sending Alert!");
-    } else {
-      Serial.println("No Human Detected: Sending Alert!");
-    }
+    digitalWrite(LEDWarning, LOW);
+  }
+  if (flameValue > 1000) { // Adjust threshold for flame detection
+    digitalWrite(LEDAlert, HIGH);
+    Blynk.logEvent("flame_detected","Flame detected!");
+    digitalWrite(relayPump, LOW); // Activate pump or alert system
+    Blynk.virtualWrite(V2, HIGH); // Check if flame detected
+    delay(5000); // Delay to prevent multiple triggers
+  } else {
+    digitalWrite(relayPump, HIGH); // Deactivate pump or alert system
+    digitalWrite(LEDAlert, LOW);
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED, OUTPUT);
+  pinMode(LEDAlert, OUTPUT);
+  pinMode(LEDWarning, OUTPUT);
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   timer.setInterval(1000L, sendSensor); // Set interval to 1 second
